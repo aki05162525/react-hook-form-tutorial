481c26144267723821712f92ff2f9c470f1c1751
aki05162525 <akihiro05162525@gmail.com>
2025-06-06 10:23:24 +0900

    form完成

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,64 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import "./App.css";
+import { useForm } from "react-hook-form";
+
+interface LoginForm {
+  name: string;
+  email: string;
+  password: string;
+}
 
 function App() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<LoginForm>({ mode: "onChange" });
+
+  const onSubmit = (data: LoginForm) => {
+    console.log(data);
+  };
+
   return (
     <div className="form-container">
-      <form>
+      <h1>Login Form</h1>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <label htmlFor="名前">名前</label>
-        <input id="name" type="text" />
+        <input
+          id="name"
+          type="text"
+          {...register("name", {
+            required: "名前は必須です",
+            minLength: {
+              value: 4,
+              message: "名前は4文字以上で入力してください",
+            },
+          })}
+        />
+        <p>{errors.name?.message as React.ReactNode}</p>
         <label htmlFor="メールアドレス">メールアドレス</label>
-        <input id="email" type="email" />
+        <input
+          id="email"
+          type="email"
+          {...register("email", {
+            required: "Emailは必須です",
+          })}
+        />
+        <p>{errors.email?.message as React.ReactNode}</p>
+
         <label htmlFor="パスワード">パスワード</label>
-        <input id="password" type="password" />
+        <input
+          id="password"
+          type="password"
+          {...register("password", {
+            required: "パスワードは必須です",
+            minLength: {
+              value: 6,
+              message: "名前は6文字以上で入力してください",
+            },
+          })}
+        />
+        <p>{errors.password?.message as React.ReactNode}</p>
 
         <button type="submit">送信</button>
       </form>
