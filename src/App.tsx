2a292df63ded012913674b59712a2e356dba3933
aki05162525 <akihiro05162525@gmail.com>
2025-06-06 10:32:11 +0900

    zodでバリデーションを管理

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import "./App.css";
 import { useForm } from "react-hook-form";
+import { validationSchema } from "./utils/validationSchema";
+import { zodResolver } from "@hookform/resolvers/zod";
 
 interface LoginForm {
   name: string;
@@ -13,7 +15,10 @@ function App() {
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm<LoginForm>({ mode: "onChange" });
+  } = useForm<LoginForm>({
+    mode: "onChange",
+    resolver: zodResolver(validationSchema),
+  });
 
   const onSubmit = (data: LoginForm) => {
     console.log(data);
@@ -24,40 +29,14 @@ function App() {
       <h1>Login Form</h1>
       <form onSubmit={handleSubmit(onSubmit)}>
         <label htmlFor="名前">名前</label>
-        <input
-          id="name"
-          type="text"
-          {...register("name", {
-            required: "名前は必須です",
-            minLength: {
-              value: 4,
-              message: "名前は4文字以上で入力してください",
-            },
-          })}
-        />
+        <input id="name" type="text" {...register("name")} />
         <p>{errors.name?.message as React.ReactNode}</p>
         <label htmlFor="メールアドレス">メールアドレス</label>
-        <input
-          id="email"
-          type="email"
-          {...register("email", {
-            required: "Emailは必須です",
-          })}
-        />
+        <input id="email" type="email" {...register("email")} />
         <p>{errors.email?.message as React.ReactNode}</p>
 
         <label htmlFor="パスワード">パスワード</label>
-        <input
-          id="password"
-          type="password"
-          {...register("password", {
-            required: "パスワードは必須です",
-            minLength: {
-              value: 6,
-              message: "名前は6文字以上で入力してください",
-            },
-          })}
-        />
+        <input id="password" type="password" {...register("password")} />
         <p>{errors.password?.message as React.ReactNode}</p>
 
         <button type="submit">送信</button>
